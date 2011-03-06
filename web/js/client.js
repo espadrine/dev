1dfe7b498be30d475260be70052d9c6298bb90ae
jan333 <jan.keromnes@gmail.com>
2011-03-06 21:16:38 +0100

    index.html updated

--- web/js/client.js
+++ web/js/client.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2011 Jan Keromnes. All rights reserved.
  * */
  
- window.client = {
+window.client = {
   user: "dom",
   rev: 0,
   lastcopy: "<!doctype html>\n<title><\/title>\n\n<body>\n  <canvas id=tutorial width=150 height=150><\/canvas>\n\n  <script>\n    var canvas = document.getElementById('tutorial');\n    var context = canvas.getContext('2d');\n\n    context.fillStyle = 'rgb(250,0,0)';\n    context.fillRect(10, 10, 55, 50);\n\n    context.fillStyle = 'rgba(0, 0, 250, 0.5)';\n    context.fillRect(30, 30, 55, 50);\n  <\/script>\n<\/body>",
@@ -22,21 +22,23 @@ window.editor = new CodeMirror(document.body, {
 window.extenditor = {
   applydelta : function(delta, editor) {
     var car = 0;
-	var line = editor.firstLine();
-	for(var i = 0 ; i < delta.length ; i++ ) {
-	  while(delta[i][2] > (car + editor.lineContent(line).length)) {
-	    car += editor.lineContent(line).length;
-		line = editor.nextLine(line);
-	  }
-	  if(delta[i][0] == 1) {
-	    //alert("insert "+JSON.stringify(delta[i]));
-		editor.insertIntoLine(line, resp.delta[i][2] - car, delta[i][1]);
-	  }
-	  else {
-	    //alert("remove "+JSON.stringify(delta[i]));
-	    editor.removeFromLine(line, resp.delta[i][2] - car, delta[i][1]);
+    var max = editor.getCode().length;
+    var line = editor.firstLine();
+    for(var i = 0 ; i < delta.length ; i++ ) {
+      while(delta[i][2] > (car + editor.lineContent(line).length)) {
+        car += editor.lineContent(line).length + 1;
+      line = editor.nextLine(line);
       }
-	}
+      var pos = (delta[i][2] - car < max ? delta[i][2] - car : "end" );
+      if(delta[i][0] == 1) {
+        //alert("insert "+JSON.stringify(delta[i]));
+      editor.insertIntoLine(line, pos, delta[i][1]);
+      }
+      else {
+        //alert("remove "+JSON.stringify(delta[i]));
+        editor.removeFromLine(line, pos, delta[i][1]);
+        }
+    }
   }
 };
 
@@ -60,16 +62,15 @@ setInterval(Scout.send(function(xhr, params){
   
   params.resp = function(xhr, resp) {
 	
-	client.rev = resp.rev;
-	client.delta = [];
-	
-	var code = editor.getCode();
-	var dmp = new diff_match_patch();
-	client.delta = Diff.solve(Diff.delta(dmp.diff_main(bufcopy, code)), resp.delta);
-	
-	extenditor.applydelta(resp.delta, editor);
-	client.lastcopy = editor.getCode();
-	extenditor.applydelta(client.delta, editor);
+    client.rev = resp.rev;
+    client.delta = [];
+    
+    var dmp = new diff_match_patch();
+    client.delta = Diff.solve(Diff.delta(dmp.diff_main(bufcopy, editor.getCode())), resp.delta);
+    
+    extenditor.applydelta(resp.delta, editor);
+    client.lastcopy = editor.getCode();
+    extenditor.applydelta(client.delta, editor);
 	
   };
   
