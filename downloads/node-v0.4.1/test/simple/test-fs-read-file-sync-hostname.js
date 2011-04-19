fc08035bdb2a9bb655e6c4a311cf3e89965b64cd
Jan <jan@jan.(none)>
2011-04-19 20:42:02 +0000

    deleted some useless downloads

--- downloads/node-v0.4.1/test/simple/test-fs-read-file-sync-hostname.js
+++
@@ -1,9 +0,0 @@
-var common = require('../common');
-var assert = require('assert');
-var fs = require('fs');
-
-// test reading from hostname
-if (process.platform === 'linux2') {
-  var hostname = fs.readFileSync('/proc/sys/kernel/hostname');
-  assert.ok(hostname.length > 0);
-}
