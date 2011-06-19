31b9a1a034f5bdb9739a9c1ce1bab678e609b5e6
jankeromnes <jan.keromnes@gmail.com>
2011-06-19 20:08:44 +0200

    Merged and renamed codemirror.plus.js. Almost stable.

--- publish/js/codeMirror.plug.js
+++
@@ -1,5 +0,0 @@
-
-function(){client.notmychange=false;window.editor=new CodeMirror(document.body,{value:window.client.lastcopy,height:"100%",width:"50%",mode:"text/html",tabMode:"indent",onChange:function(){if(client.notmychange){client.notmychange=false;}else{plugger.newcontent(content);}},});window.extenditor={applydiff:function(change,editor){for(var i=0,from={'line':0,'ch':0},to={'line':0,'ch':0};i<change.length;i++){if(change[i][0]==1){editor.replaceRange(change[i][1],from);}
-to.ch+=change[i][1].length;var rest=change[i][1].length-editor.getRange(from,to).length;while(rest>0){to.line++;to.ch=rest-1;rest=change[i][1].length-editor.getRange(from,to).length;}
-if(change[i][0]==-1){editor.replaceRange('',from,to);to.line=from.line;to.ch=from.ch;}else{from.line=to.line;from.ch=to.ch;}}}}
-var plugger=getPlug(function onnewcontent(content){client.notmychange=true;editor.setValue(resp.data);return editor.getValue();},function onnewdiff(diff){client.notmychange=true;extenditor.applydiff(diff,editor);return editor.getValue();});})();
